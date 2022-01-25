9f49cd6353793d2f5019c7f19e0ceae56cd80267
Brian Rolek <rolek.brian@peregrinefund.org>
2022-01-25 09:28:34 -0400

    Updating immigration results. Adding figures. Editing.

--- R/05-Postprocessing.R
+++ R/05-Postprocessing.R
@@ -1001,3 +1001,35 @@ sd(prod.yr, na.rm=T)
 range(prod.yr, na.rm=T)
 mean(datl$prod, na.rm=T)
 sd(datl$prod, na.rm=T)
+
+## ---- immigration ---------
+load("C:\\Users\\rolek.brian\\Documents\\Projects\\APLO IPM\\outputs\\ipm-jags-imm.Rdata")
+source("R/HDIofMCMC.R")
+options(scipen = 100)
+x <- seq(from=0, to=4, by=0.01)
+df <- data.frame(x=x, prior=dnorm(x,0,1))
+df2 <- data.frame(omega=out$sims.list$omega1)
+
+par(mar=c(5,5,2,5))
+plot(df$x, df$prior, type="l", lwd=2, xlim=c(0,4),
+     ylab="Prior density (black line)", 
+     xlab=expression(paste("Immigration rate (",omega,")", sep="")),
+     main="")
+par(new=TRUE)
+plot(density(df2$omega), type="l", lwd=2, xlim=c(0,4),
+     main="", col="blue",
+     axes = FALSE, xlab = "", ylab = "")
+axis(side = 4, at = c(0,30,60))      # Add second axis
+mtext("Posterior density (blue line)", side = 4, line = 3)  
+
+mnI <- apply(out$sims.list$NI[,3:26], 1, mean)
+median(mnI)
+HDIofMCMC(mnI)
+median(out$sims.list$omega1)
+HDIofMCMC(out$sims.list$omega1)
+
+hist(out$sims.list$omega1, main="Histogram of Immigration", xlab="Posterior draws of immigration")
+abline(v=median(out$sims.list$omega1), lwd=2)
+abline(v=HDIofMCMC(out$sims.list$omega1, cred=0.95), lty=2, lwd=2)
+median(out$sims.list$omega1)
+HDIofMCMC(out$sims.list$omega1, cred=0.95)
--- R/PosteriorPriorPlots.R
+++ R/PosteriorPriorPlots.R
@@ -1,3 +1,5 @@
+source("R/HDIofMCMC.R")
+options(scipen = 100)
 x <- seq(from=0, to=4, by=0.01)
 df <- data.frame(x=x, prior=dnorm(x,0,1))
 df2 <- data.frame(omega=out$sims.list$omega1)
@@ -5,7 +7,9 @@ df2 <- data.frame(omega=out$sims.list$omega1)
 # in R base
 par(mar=c(5,5,2,5))
 plot(df$x, df$prior, type="l", lwd=2, xlim=c(0,4),
-     ylab="Prior density (black line)", main="")
+     ylab="Prior density (black line)", 
+     xlab=expression(paste("Immigration rate (",omega,")", sep="")),
+     main="")
 par(new=TRUE)
 plot(density(df2$omega), type="l", lwd=2, xlim=c(0,4),
      main="", col="blue",
@@ -13,6 +17,11 @@ plot(density(df2$omega), type="l", lwd=2, xlim=c(0,4),
 axis(side = 4, at = c(0,30,60))      # Add second axis
 mtext("Posterior density (blue line)", side = 4, line = 3)  
 
+mnI <- apply(out$sims.list$NI[,3:26], 1, mean)
+median(mnI)
+HDIofMCMC(mnI)
+median(out$sims.list$omega1)
+HDIofMCMC(out$sims.list$omega1)
 ##########
 # plot number of immigrants each year
 ##########
